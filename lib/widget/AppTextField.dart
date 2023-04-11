c5a3078be13065c7485ba27e81749077db199f7c
NoamGini <92583248+NoamGini@users.noreply.github.com>
2023-04-11 11:23:34 +0300

    almost done register page and login page (#1)

---
+++ lib/widget/AppTextField.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+
+class AppTextField extends StatelessWidget {
+  String labelText;
+  String hintText;
+  IconData icon;
+  bool isPassword;
+  TextEditingController textController = TextEditingController();
+  AppTextField(this.labelText, this.hintText, this.icon, this.textController, this.isPassword);
+
+  @override
+  //this page has the TextField design
+  Widget build(BuildContext context) {
+
+    return TextField(
+        controller: textController,
+        obscureText: isPassword, //hide text
+        obscuringCharacter: "*",
+        textAlign: TextAlign.end,
+        decoration: InputDecoration(
+          labelText: labelText,
+          hintText: hintText,
+          prefixIcon: Icon(icon, color: Colors.black,),
+          hintStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          labelStyle: TextStyle(fontSize: 15, color: Colors.black),
+        )
+    );
+  }
+}
