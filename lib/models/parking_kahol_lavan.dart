b21bcf330bf1c9cf2a476afa430af8e031ab6a00
noaziv55 <92301625+noaziv55@users.noreply.github.com>
2023-05-31 16:31:25 +0300

    creating popup in the home page to release parking in kahol-lavan

---
+++ lib/models/parking_kahol_lavan.dart
@@ -0,0 +1,46 @@
+import 'dart:core';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class ParkingKaholLavan {
+  ParkingKaholLavan(
+      this._address,
+      this._status,
+      this._coordinates,
+      this._release_time,
+
+      );
+
+  final String _address;
+  String _status;
+  String _release_time;
+  final LatLng _coordinates;
+
+
+  LatLng get getCoordinates {
+    return _coordinates;
+  }
+
+  String get getStatus {
+    return _status;
+  }
+
+  String get getAddress {
+    return _address;
+  }
+
+  String get getReleaseTime{
+    return _release_time;
+  }
+
+  // Convert object to JSON
+  Map<String, dynamic> toJson() {
+    return {
+      'address': _address,
+      'latitude': getCoordinates.latitude,
+      'longitude': getCoordinates.longitude,
+      'release_time': _release_time,
+      'status': _status,
+    };
+  }
+
+}
